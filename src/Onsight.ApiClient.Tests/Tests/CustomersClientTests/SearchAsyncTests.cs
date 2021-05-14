ba1ae3375dd9b3f24180f0ac9a569debf87b33f0
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-05-14 10:39:50 +0200

    no message

--- src/Onsight.ApiClient.Tests/Tests/CustomersClientTests/SearchAsyncTests.cs
+++ src/Onsight.ApiClient.Tests/Tests/CustomersClientTests/SearchAsyncTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using Onsight.ApiClient.Abstractions.Commands.Customers;
 using Onsight.ApiClient.Clients;
@@ -14,11 +15,14 @@ namespace Onsight.ApiClient.Tests.Tests.CustomersClientTests
             //Act
             var result = await Sut.SearchAsync(new CustomerSearchCommand
             {
-                Query = "customer"
+                Query = "price customer"
             });
 
             //Assert
-            Assert.That(result.Results.Length, Is.EqualTo(6));
+            Assert.That(result.Results.Length, Is.EqualTo(3));
+            Assert.That(result.Results.FirstOrDefault(x => x.Name == "High AND Low price customer"), Is.Not.Null);
+            Assert.That(result.Results.FirstOrDefault(x => x.Name == "Low price customer"), Is.Not.Null);
+            Assert.That(result.Results.FirstOrDefault(x => x.Name == "High price customer"), Is.Not.Null);
 
         }
     }
