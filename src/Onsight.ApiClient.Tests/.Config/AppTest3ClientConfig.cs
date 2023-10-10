e3cc8ea006b0ed933b8883dcaadaba0c733ca74c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2023-10-10 12:56:08 +0200

    hard coded api key

--- src/Onsight.ApiClient.Tests/.Config/AppTest3ClientConfig.cs
+++ src/Onsight.ApiClient.Tests/.Config/AppTest3ClientConfig.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections.Generic;
-using Blauhaus.Analytics.Abstractions.Config;
-using Blauhaus.Analytics.Abstractions.Service;
-using Blauhaus.Common.ValueObjects.BuildConfigs;
-using Microsoft.Extensions.Configuration;
-using Onsight.ApiClient.Abstractions.Config;
+﻿using Onsight.ApiClient.Abstractions.Config;
 
 namespace Onsight.ApiClient.Tests.Config
 {
     public class AppTest3ClientConfig : OnsightApiClientConfig
     {
-        public AppTest3ClientConfig(IConfiguration config)
-            : base(config.GetSection("APPTEST3_APIKEY").Value, 
-                config.GetSection("APPTEST3_APISECRET").Value)
+        public AppTest3ClientConfig() : base(
+            apiKey: "Xg6YaWeCj9VOahCCWCPhAGkEDwfD0rSB",
+            apiSecret: "XNzCLarBtdmi1+TLvU+VPmqs/1yuYuv+")
         {
         }
 
